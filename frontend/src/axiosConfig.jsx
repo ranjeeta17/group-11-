6193de21c6a4fd83b3bfa340b4f29ed5e6a9f133
ranjeeta17 <ranjeeta.bscsf19@iba-suk.edu.pk>
2025-10-03 23:25:04 +1000

    Update axiosConfig

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 const API_BASE_URL = 'http://16.176.17.51:5001';
+//const API_BASE_URL = 'http://localhost:5001';
+
+
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
